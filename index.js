418736ced1ff162793240f520e5817a25955182b
Shehryar Abbasi <shehryar.abbasi@gmail.com>
2017-06-30 00:04:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 var ZabbixSender = require('./zabbix');
 var Sender = new ZabbixSender({host: 'ZabbixPla-ZabbixAu-1P80076K4P8M7-1965525499.us-east-2.elb.amazonaws.com'});
-
-// Add items to request
-//Sender.addItem('webserver', 'httpd.running', 0);
-//Sender.addItem('dbserver', 'mysql.ping', 1);
-
-// Add item with default host
-Sender.addItem('MikeTemplateItemKey', 350);
-
-// Send the items to zabbix trapper
-Sender.send(function(err, res) {
-    if (err) {
-        throw err;
-    }
-
-    // print the response object
-    console.dir(res);
-});
+exports.handler = (event, context, callback) => {
+    // Add items to request
+    //Sender.addItem('webserver', 'httpd.running', 0);
+    //Sender.addItem('dbserver', 'mysql.ping', 1);
+    
+    // Add item with default host
+    Sender.addItem('lambda','MikeTemplateItemKey', 350);
+    
+    // Send the items to zabbix trapper
+    Sender.send(function(err, res) {
+        if (err) {
+            throw err;
+        }
+    
+        // print the response object
+        console.log(res);
+        
+        callback(null, res);
+    });
+    
+};
